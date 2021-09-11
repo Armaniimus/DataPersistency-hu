93591850248bbc3a55b7447d20207c9152faada7
Armaniimus_home <Tommy_chester@hotmail.com>
2021-09-11 15:35:12 +0200

    OVChipkaartDAOPsql toegevoegt en implimenteerd nu bijbehorende interface

---
+++ practicum4/src/OVChipkaartDAOPsql.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OVChipkaartDAOPsql implements OVChipkaartDAO {
+    public boolean save(OVChipkaart ovChipkaart) {
+        try {
+            return false;
+        } catch(Exception err) {
+            System.err.println("OVCHiplaartDAOPsql geeft een error in save(): " + err.getMessage() );
+            return false;
+        }
+    }
+
+    public boolean update(OVChipkaart ovChipkaart) {
+        try {
+            return false;
+        } catch(Exception err) {
+            System.err.println("OVCHiplaartDAOPsql geeft een error in update(): " + err.getMessage() );
+            return false;
+        }
+    }
+
+    public boolean delete(OVChipkaart ovChipkaart) {
+        try {
+            return false;
+        } catch(Exception err) {
+            System.err.println("OVCHiplaartDAOPsql geeft een error in delete(): " + err.getMessage() );
+            return false;
+        }
+    }
+
+    public List<OVChipkaart> findByReiziger(Reiziger reiziger) {
+        List<OVChipkaart> OVChipkaartArray = new ArrayList<>();
+        try {
+
+        } catch(Exception err) {
+            System.err.println("OVCHiplaartDAOPsql geeft een error in findByReiziger(): " + err.getMessage() );
+        }
+
+        return OVChipkaartArray;
+    }
+
+    public List<OVChipkaart> findAll() {
+        List<OVChipkaart> OVChipkaartArray = new ArrayList<>();
+        try {
+
+        } catch(Exception err) {
+            System.err.println("OVCHiplaartDAOPsql geeft een error in findAll(): " + err.getMessage() );
+        }
+
+        return OVChipkaartArray;
+    }
+}
