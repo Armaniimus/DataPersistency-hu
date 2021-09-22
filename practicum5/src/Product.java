188c5d3277da05fb4af59632631ed24dc890d4d8
Armaniimus_home <Tommy_chester@hotmail.com>
2021-09-23 00:02:56 +0200

    added product

---
+++ practicum5/src/Product.java
@@ -0,0 +1,69 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class Product {
+    private int product_nummer;
+    private String naam;
+    private String beschrijving;
+    private Double prijs;
+    private ArrayList<OVChipkaart> OvChipkaartList;
+
+    public Product( int product_nummer, String naam, String beschrijving, Double prijs) {
+        this.product_nummer=product_nummer;
+        this.naam = naam;
+        this.beschrijving = beschrijving;
+        this.prijs = prijs;
+    }
+
+    public String toString() {
+        String OvChipkaartListString = "";
+        if (this.OvChipkaartList != null && !this.OvChipkaartList.isEmpty()) {
+            OvChipkaartListString += "OvChipkaartenList[";
+            for (int i=0; i <this.OvChipkaartList.size(); i++) {
+                if (i > 1) {
+                    OvChipkaartListString += ", ";
+                }
+                OvChipkaartListString += " OvChipkaart" + this.OvChipkaartList.get(i).getInfo();
+            }
+            OvChipkaartListString += " ]";
+        } else {
+            OvChipkaartListString = "null";
+        }
+
+        String resultString = "Product{ ";
+        resultString += __internalGetInfo() + ", ";
+        resultString += OvChipkaartListString;
+        resultString += " }";
+
+        return resultString;
+    }
+
+    public String getInfo() {
+        return  "{ " + this.__internalGetInfo() + " }";
+    }
+
+    private String __internalGetInfo() {
+        String reizigerStr = "";
+        reizigerStr += this.product_nummer + ", ";
+        reizigerStr += this.naam + ", ";
+        reizigerStr += this.beschrijving + ", ";
+        reizigerStr += this.prijs;
+        return reizigerStr;
+    }
+
+    public int getProduct_nummer() { return product_nummer; }
+    public void setProduct_nummer(int product_nummer) { this.product_nummer = product_nummer; }
+
+    public String getNaam() { return naam; }
+    public void setNaam(String naam) { this.naam = naam; }
+
+    public String getBeschrijving() { return beschrijving; }
+    public void setBeschrijving(String beschrijving) { this.beschrijving = beschrijving; }
+
+    public Double getPrijs() { return prijs; }
+    public void setPrijs(Double prijs) { this.prijs = prijs; }
+
+    public ArrayList<OVChipkaart> getOvChipkaartList() { return OvChipkaartList; }
+    public void setOvChipkaartList(ArrayList<OVChipkaart> ovChipkaartList) { OvChipkaartList = ovChipkaartList; }
+}
